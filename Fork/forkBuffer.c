bf916dd34824b5d7047744b1a26b6d596fdc8580
Kartik Siddhabathula <ksiddhabathula@apple.com>
2019-10-05 09:36:47 -0700

    Program to show the buffering of the fork

--- Fork/forkBuffer.c
+++ Fork/forkBuffer.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 
+/*
+	Compile this program and run it twice
+	1. Output to the standard output
+	2. Output to a file
+
+	In the standard output, Before fork line is printed once
+	When the output redirected to a file, Before fork is printed twice
+
+*/
+
 int globvar = 6;
 char buf[] ="a write to stdout\n";
 
