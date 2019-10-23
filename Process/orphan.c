1f0df965ebf11e0d2615e0b00f97c9e731cb9a87
Kartik Siddhabathula <ksiddhabathula@apple.com>
2019-10-23 08:04:32 -0700

    This program demonstrates the orphan process

--- Process/orphan.c
+++ Process/orphan.c
@@ -12,18 +12,17 @@ void main()
 	if(pid == 0)
 	{
 		printf("I am the child, my process ID is %d\n",getpid());
-		printf("I am the child's parent, my process ID is %d\n",getppid());
+		printf("My parent's process ID is %d\n",getppid());
 		sleep(30);
 		printf("\nAfter sleep\nI am the child, my process ID is %d\n",getpid());
-                printf("I am the child's parent, my process ID is %d\n",getppid());
-		printf("Again going to sleep for 10 secs");
-		sleep(10);
+                printf("My parent's process ID is %d\n",getppid());
+		exit(0);
 	}
 	else
 	{
 		sleep(20);
 		printf("I am the parent, my process ID is %d\n",getpid());
-                printf("The parent's parent, my process ID is %d\n",getppid());
+                printf("The parent's parent, process ID is %d\n",getppid());
 		printf("Parent terminates\n");
 	}
 }
